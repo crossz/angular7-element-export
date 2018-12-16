eb1d07bdd0905601f08f6753faa1b8217644197a
郑欣 <zhengxin@zhengxindeiMac.local>
2018-12-16 20:19:43 +0800

    from angular6 upgraded to angular7.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
+import { JsaCounterComponent } from './jsa-counter/jsa-counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsaCounterComponent
+  ],
+  entryComponents: [
+    JsaCounterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const element = createCustomElement(JsaCounterComponent, { injector: this.injector });
+    customElements.define('jsa-counter', element);
+  }
+}
